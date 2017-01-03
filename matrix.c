c9a99df50c9961103408739fde65d84c60d40404
Yuriy Nazarov <nazarov.yuriy.pavlovich@gmail.com>
2017-01-04 01:48:42 +0400

    Matrix multiplication 3.8 sec With 4 same operations

--- matrix.c
+++ matrix.c
@@ -1,5 +1,4 @@
-void matrix_multiply(double *a, double *b, double *c, int n) {
-    //Transpose b
+void __attribute__ ((noinline)) transpose(double *b, int n) {
     for (int i = 0; i < n; i++) {
         for (int j = i + 1; j < n; j++) {
             double t = b[i * n + j];
@@ -7,14 +6,26 @@ void matrix_multiply(double *a, double *b, double *c, int n) {
             b[j * n + i] = t;
         }
     }
+}
 
+void matrix_multiply(double *a, double *b, double *c, int n) {
+    transpose(b, n);
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            double t = 0;
+        for (int j = 0; j < n; j += 4) {
+            double t0 = 0;
+            double t1 = 0;
+            double t2 = 0;
+            double t3 = 0;
             for (int k = 0; k < n; k++) {
-                t += a[i * n + k] * b[j * n + k];
+                t0 += a[i * n + k] * b[(j + 0) * n + k];
+                t1 += a[i * n + k] * b[(j + 1) * n + k];
+                t2 += a[i * n + k] * b[(j + 2) * n + k];
+                t3 += a[i * n + k] * b[(j + 3) * n + k];
             }
-            c[i * n + j] = t;
+            c[i * n + j + 0] = t0;
+            c[i * n + j + 1] = t1;
+            c[i * n + j + 2] = t2;
+            c[i * n + j + 3] = t3;
         }
     }
 }
