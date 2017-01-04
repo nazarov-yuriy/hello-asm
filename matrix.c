00f2c3052032d8b2e2394919d2c701ef6b7130b3
Yuriy Nazarov <nazarov.yuriy.pavlovich@gmail.com>
2017-01-05 02:25:32 +0400

    Matrix multiplication 5.2 sec Kind of bugfix

--- matrix.c
+++ matrix.c
@@ -1,44 +1,42 @@
 #include <immintrin.h>
 
-void __attribute__ ((noinline)) transpose(double *b, int n) {
-    for (int i = 0; i < n; i++) {
-        for (int j = i + 1; j < n; j++) {
-            double t = b[i * n + j];
-            b[i * n + j] = b[j * n + i];
-            b[j * n + i] = t;
+void __attribute__ ((noinline)) transpose4(double *m, int n) {
+    for (int i = 0; i < n; i += 4) {
+        for (int j = i + 4; j < n; j += 4) {
+            __m256d a = _mm256_load_pd(&m[(i + 0) * n + j]);
+            __m256d b = _mm256_load_pd(&m[(i + 1) * n + j]);
+            __m256d c = _mm256_load_pd(&m[(i + 2) * n + j]);
+            __m256d d = _mm256_load_pd(&m[(i + 3) * n + j]);
+
+            __m256d e = _mm256_load_pd(&m[(j + 0) * n + i]);
+            __m256d f = _mm256_load_pd(&m[(j + 1) * n + i]);
+            __m256d g = _mm256_load_pd(&m[(j + 2) * n + i]);
+            __m256d h = _mm256_load_pd(&m[(j + 3) * n + i]);
+
+            _mm256_store_pd(&m[(j + 0) * n + i], a);
+            _mm256_store_pd(&m[(j + 1) * n + i], b);
+            _mm256_store_pd(&m[(j + 2) * n + i], c);
+            _mm256_store_pd(&m[(j + 3) * n + i], d);
+
+            _mm256_store_pd(&m[(i + 0) * n + j], e);
+            _mm256_store_pd(&m[(i + 1) * n + j], f);
+            _mm256_store_pd(&m[(i + 2) * n + j], g);
+            _mm256_store_pd(&m[(i + 3) * n + j], h);
         }
     }
 }
 
 void matrix_multiply(double *a, double *b, double *c, int n) {
-    transpose(b, n);
+    transpose4(b, n);
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            __m256d t0 = _mm256_set_pd(0, 0, 0, 0);
-            __m256d t1 = _mm256_set_pd(0, 0, 0, 0);
-            __m256d t2 = _mm256_set_pd(0, 0, 0, 0);
-            __m256d t3 = _mm256_set_pd(0, 0, 0, 0);
-            for (int k = 0; k < n; k += 16) {
-                __m256d a0 = _mm256_load_pd(&a[i * n + k + 0]);
-                __m256d a1 = _mm256_load_pd(&a[i * n + k + 4]);
-                __m256d a2 = _mm256_load_pd(&a[i * n + k + 8]);
-                __m256d a3 = _mm256_load_pd(&a[i * n + k + 12]);
-
-                __m256d b0 = _mm256_load_pd(&b[j * n + k + 0]);
-                __m256d b1 = _mm256_load_pd(&b[j * n + k + 4]);
-                __m256d b2 = _mm256_load_pd(&b[j * n + k + 8]);
-                __m256d b3 = _mm256_load_pd(&b[j * n + k + 12]);
-
-                t0 = _mm256_fmadd_pd(a0, b0, t0);
-                t1 = _mm256_fmadd_pd(a1, b1, t1);
-                t2 = _mm256_fmadd_pd(a2, b2, t2);
-                t3 = _mm256_fmadd_pd(a3, b3, t3);
+        for (int j = 0; j < n; j += 4) {
+            __m256d t4 = _mm256_set_pd(0, 0, 0, 0);
+            for (int k = 0; k < n; k++) {
+                __m256d a4 = _mm256_set_pd(a[i * n + k], a[i * n + k], a[i * n + k], a[i * n + k]);
+                __m256d b4 = _mm256_load_pd(&b[j * n + k * 4]);
+                t4 = _mm256_fmadd_pd(a4, b4, t4);
             }
-            c[i * n + j + 0] =
-                    ((double *) &t0)[0] + ((double *) &t0)[1] + ((double *) &t0)[2] + ((double *) &t0)[3] +
-                    ((double *) &t1)[0] + ((double *) &t1)[1] + ((double *) &t1)[2] + ((double *) &t1)[3] +
-                    ((double *) &t2)[0] + ((double *) &t2)[1] + ((double *) &t2)[2] + ((double *) &t2)[3] +
-                    ((double *) &t3)[0] + ((double *) &t3)[1] + ((double *) &t3)[2] + ((double *) &t3)[3];
+            _mm256_store_pd(&c[i * n + j], t4);
         }
     }
 }
