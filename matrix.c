c67857481d2c9bd0f13709b6b0535e30330197b5
Yuriy Nazarov <nazarov.yuriy.pavlovich@gmail.com>
2017-01-07 01:27:24 +0400

    Matrix multiplication 2.0 sec Tiled multiplication. Another layout.

--- matrix.c
+++ matrix.c
@@ -16,25 +16,29 @@ void matrix_multiply(double *a, double *b, double *c, int n) {
     for (int ti = 0; ti < n / si; ti++) {
         for (int tj = 0; tj < n / sj; tj++) {
             for (int pi = 0; pi < si; pi++) {
-                for (int pj = 0; pj < sj; pj += 4) {
+                for (int pj = 0; pj < sj; pj += 8) {
                     int i = ti * si + pi;
                     int j = tj * sj + pj;
                     double t0 = 0, t1 = 0, t2 = 0, t3 = 0;
                     double t4 = 0, t5 = 0, t6 = 0, t7 = 0;
-                    for (int k = 0; k < n; k += 2) {
+                    for (int k = 0; k < n; k++) {
                         t0 += a[i * n + k + 0] * b[(j + 0) * n + k + 0];
                         t1 += a[i * n + k + 0] * b[(j + 1) * n + k + 0];
                         t2 += a[i * n + k + 0] * b[(j + 2) * n + k + 0];
                         t3 += a[i * n + k + 0] * b[(j + 3) * n + k + 0];
-                        t4 += a[i * n + k + 1] * b[(j + 0) * n + k + 1];
-                        t5 += a[i * n + k + 1] * b[(j + 1) * n + k + 1];
-                        t6 += a[i * n + k + 1] * b[(j + 2) * n + k + 1];
-                        t7 += a[i * n + k + 1] * b[(j + 3) * n + k + 1];
+                        t4 += a[i * n + k + 0] * b[(j + 4) * n + k + 0];
+                        t5 += a[i * n + k + 0] * b[(j + 5) * n + k + 0];
+                        t6 += a[i * n + k + 0] * b[(j + 6) * n + k + 0];
+                        t7 += a[i * n + k + 0] * b[(j + 7) * n + k + 0];
                     }
-                    c[i * n + j + 0] = t0 + t4;
-                    c[i * n + j + 1] = t1 + t5;
-                    c[i * n + j + 2] = t2 + t6;
-                    c[i * n + j + 3] = t3 + t7;
+                    c[i * n + j + 0] = t0;
+                    c[i * n + j + 1] = t1;
+                    c[i * n + j + 2] = t2;
+                    c[i * n + j + 3] = t3;
+                    c[i * n + j + 4] = t4;
+                    c[i * n + j + 5] = t5;
+                    c[i * n + j + 6] = t6;
+                    c[i * n + j + 7] = t7;
                 }
             }
         }
